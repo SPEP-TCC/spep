f406c0fdb7677182654c462b4a49bb9979873c38
vitoria.goncalves <vitoria.goncalves@brtec.org.br>
2025-02-14 19:18:45 -0400

    ajustes gerais

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  before_action :authenticate_user!
+
+  layout :layout_by_resource
+
   allow_browser versions: :modern
   include Pagy::Backend
+
+  def layout_by_resource
+    devise_controller? ? "devise" : "application"
+  end
+
 end
